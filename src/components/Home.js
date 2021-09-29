a72b531fd6806338d31c729a43712b6ba4d8c884
Daniel Dominguez <daniel.i.d.ch@gmail.com>
2021-09-29 15:35:00 -0600

    Adding load more button and fix vulnerabilities

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from './Spinner';
 import SearchBar from "./SearchBar";
+import Button from "./Bottom";
 
 //Hook
 import {useHomeFetch} from '../hooks/useHomeFetch';
@@ -14,8 +15,14 @@ import {useHomeFetch} from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const {state, loading, error, searchTerm, setSearchTerm} = useHomeFetch();
-
+    const {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore} = useHomeFetch();
+    if(error){
+        return (
+            <div>
+                Something went wrong :(
+            </div>
+        )
+    }
 
     console.log(state);
     return (
@@ -43,7 +50,10 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-            <Spinner/>
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load More' callback={() => setIsLoadingMore(true)}/>
+            )}
         </>
 
     );
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -13,13 +13,13 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const[loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
-
-    const fetchMovies = async (page, searchTerm='')=>{
+    const fetchMovies = async (page, term='')=>{
         try {
             setError(false);
             setLoading(true);
-            const movies = await API.fetchMovies(searchTerm, page);
+            const movies = await API.fetchMovies(term, page);
             console.log(movies);
             setState(prev=> ({
                 ...movies,
@@ -38,5 +38,17 @@ export const useHomeFetch = () => {
         setState(initialState);
         fetchMovies(1, searchTerm)
     }, [searchTerm]);
-    return{state: state, loading, error, searchTerm, setSearchTerm }
-}
+
+    useEffect(()=>{
+       if(!isLoadingMore) {
+        return;
+       }
+        fetchMovies(
+            state.page + 1,
+            searchTerm)
+            .then(response => setIsLoadingMore(false));
+
+
+    }, [isLoadingMore, searchTerm, state.page]);
+     return{state: state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore }
+};
